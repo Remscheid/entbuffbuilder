32d42d367ef8bac627ebb6f4479e9816bd8a3352
Remscheid <p.remscheid@yahoo.com>
2024-10-03 15:24:56 +0200

    Added doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
